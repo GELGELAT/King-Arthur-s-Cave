7dee329c26f26bec94bbb66823e885d52bd86bd7
tigorsun <87004057+GELGELAT@users.noreply.github.com>
2026-09-09 14:56:52 +0300

    compiltest

--- build/Base/Mics/Allocator/Allocator.d
+++
@@ -1,3 +0,0 @@
-build/Base/Mics/Allocator/Allocator.o: Base/Mics/Allocator/Allocator.c \
- Base/Mics/Allocator/Allocator.h
-Base/Mics/Allocator/Allocator.h:
--- build/Base/structurs.d
+++
@@ -1,5 +0,0 @@
-build/Base/structurs.o: Base/structurs.c Base/structurs.h \
- Base/RayLibs/raylib.h Base/Mics/Allocator/Allocator.h
-Base/structurs.h:
-Base/RayLibs/raylib.h:
-Base/Mics/Allocator/Allocator.h:
